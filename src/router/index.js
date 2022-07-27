bb2d1d57c627b107726a9f41316106bc17ef77b2
wjbuena <wilsonjeffrey.buena@synpulse.com>
2022-07-27 13:17:53 +0800

    Updated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     component: AboutView
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  // Re-direct
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // Catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,11 @@
 <template>
-  
+    <h1>Job Details Page</h1>
+    <p>The job ID is {{ id }}</p>
 </template>
 
 <script>
 export default {
-
+   props: ['id']
 }
 </script>
 
